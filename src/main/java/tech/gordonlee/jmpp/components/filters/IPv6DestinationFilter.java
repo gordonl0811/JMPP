0ec335af27f7b0679232c7173640d62c8bf953aa
gordonl0811 <gordonl0811@gmail.com>
2022-06-11 01:10:56 +0100

    IPv6DestinationFilter implemented without tests.

--- src/main/java/tech/gordonlee/jmpp/components/filters/IPv6DestinationFilter.java
+++ src/main/java/tech/gordonlee/jmpp/components/filters/IPv6DestinationFilter.java
@@ -8,23 +8,24 @@ import tech.gordonlee.jmpp.components.Component;
 import tech.gordonlee.jmpp.utils.PacketEvent;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import static tech.gordonlee.jmpp.utils.Utils.startDisruptor;
 
-public class IPv4DestinationFilter extends Component {
+public class IPv6DestinationFilter extends Component {
 
     private final Disruptor<PacketEvent> inputDisruptor;
     private final Disruptor<PacketEvent> outputDisruptor;
 
-    private final int addrLowerBound;
-    private final int addrUpperBound;
+    private final BigInteger addrLowerBound;
+    private final BigInteger addrUpperBound;
 
-    public IPv4DestinationFilter(Disruptor<PacketEvent> inputDisruptor, Disruptor<PacketEvent> outputDisruptor, String addrLower, String addrUpper) throws UnknownHostException {
+    public IPv6DestinationFilter(Disruptor<PacketEvent> inputDisruptor, Disruptor<PacketEvent> outputDisruptor, String addrLower, String addrUpper) throws UnknownHostException {
 
-        this.addrLowerBound = encodeIPv4Address(addrLower);
-        this.addrUpperBound = encodeIPv4Address(addrUpper);
+        this.addrLowerBound = encodeIPv6Address(addrLower);
+        this.addrUpperBound = encodeIPv6Address(addrUpper);
 
         this.inputDisruptor = inputDisruptor;
         this.outputDisruptor = outputDisruptor;
@@ -46,28 +47,21 @@ public class IPv4DestinationFilter extends Component {
 
     @Override
     public void process(Packet packet) throws IOException {
-        int addrValue = encodeIPv4Address(((IPv4Packet) packet.getPacket(Protocol.IPv4)).getDestinationIP());
-        if (addrValue >= addrLowerBound && addrValue <= addrUpperBound) {
+        BigInteger addrValue = encodeIPv6Address(((IPv4Packet) packet.getPacket(Protocol.IPv6)).getDestinationIP());
+        if (addrValue.compareTo(addrLowerBound) >= 0 && addrValue.compareTo(addrUpperBound) <= 0) {
             outputDisruptor.publishEvent((event, sequence) -> event.setValue(packet));
         }
     }
 
     /**
-     * Parse the address using the InetAddress class and convert it to an integer.
+     * Parse the address using the InetAddress class and convert it to a BigInteger.
+     * BigInteger is necessary because of the large size of IPv6 addresses.
      *
-     * @param address the IPv4 address as a string
+     * @param address the IPv6 address as a string
      * @return the address as a unique integer representation
-     * @throws UnknownHostException invalid IPv4 address
+     * @throws UnknownHostException invalid IPv6 address
      */
-    private int encodeIPv4Address(String address) throws UnknownHostException {
-
-        int result = 0;
-
-        // Encode each octet
-        for (byte b : InetAddress.getByName(address).getAddress()) {
-            result = result << 8 | (b & 0xFF);
-        }
-        return result;
-
+    private BigInteger encodeIPv6Address(String address) throws UnknownHostException {
+        return new BigInteger(1, InetAddress.getByName(address).getAddress());
     }
 }
